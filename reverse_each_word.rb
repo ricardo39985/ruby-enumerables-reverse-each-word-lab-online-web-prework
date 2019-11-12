918f5514d8ed58a2248b876bc88db1ad1362d92e
Ricardo Persaud <ricardo39985@gmail.com>
2019-11-12 23:17:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,5 @@
 def reverse_each_word(input_to_reverse)
   splitted = input_to_reverse.split(" ")
   empty_array =[]
-  for i in splitted do
-    empty_array.push(i.reverse)
-  end
-  empty_array.join(" ")
+  splitted.collect { |e| e.reverse }
 end
