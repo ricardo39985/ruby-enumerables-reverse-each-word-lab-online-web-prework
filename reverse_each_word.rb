38fe54162160396ca29bdaa7916ba5624f3667e9
Ricardo Persaud <ricardo39985@gmail.com>
2019-11-12 23:04:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word(input_to_reverse)
-  result = input_to_reverse.split(" ").each {|n|n.reverse }
-  result.each {|n| n.reverse}
+  result = input_to_reverse.split(" ")
+  result
 end
